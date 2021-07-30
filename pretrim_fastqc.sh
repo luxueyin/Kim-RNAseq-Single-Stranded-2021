d5de2ebdffda8d3d3dcfc3bb106c19bc93b239e6
Michelle Lu <74755460+luxueyin@users.noreply.github.com>
2021-07-30 06:39:19 -0700

    Create pretrim_fastqc.sh

---
+++ pretrim_fastqc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#SBATCH -c 1
+#SBATCH --mem-per-cpu=1000
+#SBATCH --job-name=PreTrimFastqc
+#SBATCH --output=PreTrimFastqc.out
+#SBATCH --time=2:00:00
+
+#######################################################################################
+#fastqc and then combine reports into one html and save to outputs folder
+
+#######################################################################################
+mkdir -p output/pretrim
+
+for sample in `cat SRR_Acc_List.txt`
+do
+
+echo ${sample} "starting"
+
+fastqc SRA/${sample}.fastq.gz --outdir output/pretrim
+
+echo ${sample} "finished"
+
+
+done
+
+
+
+#######################################################################################
+#combine
+#######################################################################################
+
+multiqc output/pretrim --filename output/PretrimFastQC_multiqc_report.html --ignore-samples Undetermined* --interactive
